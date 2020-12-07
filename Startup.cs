3dfaa245d1b2fd7819648dbf1237c7fd33cdaf1e
Exequiel Beker <exequielbeker@gmail.com>
2020-12-07 18:16:12 -0300

    Added UI to admin page

--- Startup.cs
+++ Startup.cs
@@ -19,6 +19,8 @@ namespace UserServices
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
+
             //Add Identity Server
             services.AddIdentityServer()
                 .AddInMemoryClients(Config.GetClients())
@@ -37,15 +39,15 @@ namespace UserServices
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseStaticFiles();
+
             app.UseRouting();
             app.UseIdentityServer();
+            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapDefaultControllerRoute();
             });
         }
     }
